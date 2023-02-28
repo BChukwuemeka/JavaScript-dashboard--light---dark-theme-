fb22aa8c3c69255c9379464d86124ead825af11b
Chukwuemeka <emeka4u123000@gmail.com>
2023-02-28 13:38:15 +0100

    javaScript dashboard

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders= [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'declined'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'DJI Air Drone',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00821',
+        paymentStatus: 'Paid',
+        shipping: 'delivered'
+    }
+]
